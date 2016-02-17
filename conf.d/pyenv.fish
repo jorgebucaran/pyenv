b1963f3d8a41a39c1f0ca0424246a2821f0dcc5f
Daniele Sluijters <daenney@users.noreply.github.com>
2016-02-17 23:31:01 +0100

    conf.d: Only create directories if necessary
    By checking for the existence of either directory we can be relatively
    sure that both exist or not. If one don't we just fire the `mkdir` for
    both as that won't cost us anything extra.

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,4 +1,6 @@
 set -l pyenv_root (pyenv root)
 set -x PATH $pyenv_root/shims $PATH
 set -x PYENV_SHELL fish
-command mkdir -p $pyenv_root/{shims,versions}
+if test ! -d "$pyenv_root/shims"; or test ! -d "$pyenv_root/versions"
+    command mkdir -p $pyenv_root/{shims,versions}
+end
