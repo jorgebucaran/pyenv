c8c3d4c0247ddd8b6ed95451224f31dc3ea3b4cc
Daniele Sluijters <daenney@users.noreply.github.com>
2016-02-17 23:59:20 +0100

    conf.d: Check if pyenv is installed first

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,3 +1,8 @@
+if not command -s pyenv > /dev/null
+    echo "pyenv: command not found. See https://github.com/yyuu/pyenv"
+    exit 1
+end
+
 set -l pyenv_root (pyenv root)
 set -x PATH $pyenv_root/shims $PATH
 set -x PYENV_SHELL fish
