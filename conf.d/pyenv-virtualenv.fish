c9aaf7c12b1668250f32eaf3e46a4d4803de14c8
Leira Hua <leira@apple.com>
2016-06-29 19:46:52 -0700

    dynamic path

--- conf.d/pyenv-virtualenv.fish
+++ conf.d/pyenv-virtualenv.fish
@@ -1,4 +1,5 @@
-set -x PATH '/usr/local/Cellar/pyenv-virtualenv/20160315/shims' $PATH;
+set -l pyenv_virtualenv_path (string split -r -m2 '/' (realpath (type -p pyenv-virtualenv-init)))
+set -x PATH "$pyenv_virtualenv_path[1]/shims" $PATH;
 set -x PYENV_VIRTUALENV_INIT 1;
 
 
