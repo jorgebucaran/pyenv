50f85932eb72b1513b5f67fa01bfa28dba52f274
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-11 02:55:54 -0600

    Get sh-prefixed commands directly from pyenv
    This fixes pyenv-virtualenv support, as the `pyenv` function would not properly call `sh-activiate` and `sh-deactivate`.
    
    Also removed an unnecessary recursion into our own function definition.

--- functions/pyenv.fish
+++ functions/pyenv.fish
@@ -1,10 +1,12 @@
+set __pyenv_sh_commands (command pyenv commands --sh)
+
 function pyenv
     set command $argv[1]
     set -e argv[1]
 
     switch "$command"
-        case rehash shell
-            source (pyenv "sh-$command" $argv | psub)
+        case $__pyenv_sh_commands
+            source (command pyenv "sh-$command" $argv | psub)
 
         case \*
             command pyenv "$command" $argv
